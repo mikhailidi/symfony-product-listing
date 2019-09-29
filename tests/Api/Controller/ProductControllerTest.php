409c93f1159efdacef0436b0609dac0bcbaed61a
mikhailidi <mikhailidi.p@gmail.com>
2019-09-29 14:25:27 +0200

    Implement basic Product API test case
    [ADD] ProductControllerTest
    [ADD] Basic test for product creation with basic fields: name & description

---
+++ tests/Api/Controller/ProductControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Tests\Api\Controller;
+
+use App\Tests\Api\ApiTestCase;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ProductControllerTest
+ * @package App\Tests\Api\Controller
+ */
+class ProductControllerTest extends ApiTestCase
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    /**
+     * @test
+     */
+    public function test_create_product_successful()
+    {
+        // TODO: trigger image and tags
+
+        $productData = [
+            'name' => 'First product',
+            'description' => 'Product description',
+        ];
+
+        $this->client->request('POST', '/api/products', $productData);
+
+        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
+
+
+        /**
+         * Assert data was successfully insert to the database
+         */
+        $product = $this->entityManager
+            ->getRepository(Product::class)
+            ->find('created-id-here');
+
+        $expectedProductData = [
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+        ];
+        $this->assertEquals($productData, $expectedProductData);
+    }
+}
